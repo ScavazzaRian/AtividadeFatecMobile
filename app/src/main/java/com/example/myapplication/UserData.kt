28a3abd9c6a4946e08d2ae193c771c7ad601673f
ScavazzaRian <rianscavazza.git@gmail.com>
2025-12-10 19:47:23 -0300

    chore

---
+++ app/src/main/java/com/example/myapplication/UserData.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+data class UserData(
+    val nome: String? = null,
+    val email: String? = null,
+    val idade: Int? = null,
+    val celular: String? = null,
+    val senha: String? = null,
+)
